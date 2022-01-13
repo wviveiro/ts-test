74a64aec12a9c7f5abf171098443c48c67f0ea58
Wellington Viveiro <wellyviveiro@gmail.com>
2022-01-13 20:11:23 +1100

    Implement getArnieQuotes

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,28 @@
-import { httpGet } from './mock-http-interface';
+import { httpGet } from "./mock-http-interface";
 
-// TODO define this type properly
-type TResult = any;
+interface TResult {
+  [key: string]: string;
+}
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+interface FetchedResultsInterface {
+  [key: string]: Promise<{ status: number; body: string }>;
+}
+
+/** Used to chache data already fetched, so httpget is not called twice */
+const fetchedResults: FetchedResultsInterface = {};
+
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+  const results = [];
+  for (let i = 0; i < urls.length; i++) {
+    if (!fetchedResults[urls[i]]) {
+      fetchedResults[urls[i]] = httpGet(urls[i]);
+    }
+
+    results.push(await fetchedResults[urls[i]]);
+  }
+
+  return results.map((result) => ({
+    [result.status === 200 ? "Arnie Quote" : "FAILURE"]: JSON.parse(result.body)
+      .message,
+  }));
 };
